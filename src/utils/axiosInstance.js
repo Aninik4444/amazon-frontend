7697dea516772dee8107505adc9584114907b74c
Aninik4444 <nikitakumarisnd2000@gmail.com>
2025-05-28 23:40:21 +0530

    v2 api

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import store from './store';
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api/v1`,
+  baseURL: `${process.env.REACT_APP_API_URL}/api/v2`,
 });
 
 axiosInstance.interceptors.request.use((req) => {
